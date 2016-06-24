8b7b028c85cc543e957b5249b18de5398e646646
Zach Stearman <zstearman3@gmail.com>
2016-06-24 21:23:33 +0000

    Add testing for login logout functionality from excercizes.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:example)
+    @other_user = users(:snow)
+  end
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,8 +15,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
-    
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+    assert_select "a[href=?]", logout_path, count: 0
     get signup_path
     assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
+  end
+  
+  test "links appear when signed in" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -49,5 +49,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+    assert_nil session[:forwarding_url]
   end
 end
