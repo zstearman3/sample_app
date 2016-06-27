62479b5f4f978412080fe377f36346d2b4a03719
Zach Stearman <zstearman3@gmail.com>
2016-06-27 21:17:13 +0000

    Correct error in production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'stearmantweettweet.herokuapp.com'
-  config.action_mailer.default_url_option = { host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net'
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME']
-    :password       => ENV['SENDGRID_PASSWORD']
-    :domain         => 'heroku.com'
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 
