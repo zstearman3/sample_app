2c36be3dd4449e1e064c58496d39ca3e8da61849
Zach Stearman <zstearman3@gmail.com>
2016-06-28 18:32:03 +0000

    Add delete method to microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: [:destroy]
   
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,11 +8,25 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
-      flash[:danger] = "Something went wrong"
     end
   end
   
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted!"
+    redirect_to request.referrer || root_url
   end
+  
+  private
+  
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
 end
