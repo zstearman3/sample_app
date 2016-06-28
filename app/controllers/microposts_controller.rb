366f7520c43cc9ef73e62e14041b0c794d071e6f
Zach Stearman <zstearman3@gmail.com>
2016-06-28 16:27:58 +0000

    Add micropost field to home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,17 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+      flash[:danger] = "Something went wrong"
+    end
+  end
+  
+  def destroy
+  end
 end
